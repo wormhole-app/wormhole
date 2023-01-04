a45957f84045268a47ff7a77b289bc0f7621e67d
lukas-heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2023-01-04 11:46:49 +0100

    add basic sendfile methhod

--- native/src/api.rs
+++ native/src/api.rs
@@ -2,58 +2,138 @@
 // When adding new code to your project, note that only items used
 // here will be transformed to their Dart equivalents.
 
-// A plain enum without any fields. This is similar to Dart- or C-style enums.
-// flutter_rust_bridge is capable of generating code for enums with fields
-// (@freezed classes in Dart and tagged unions in C).
-pub enum Platform {
-    Unknown,
-    Android,
-    Ios,
-    Windows,
-    Unix,
-    MacIntel,
-    MacApple,
-    Wasm,
-}
+use std::borrow::Cow;
+use std::time::Duration;
+use magic_wormhole::rendezvous::DEFAULT_RENDEZVOUS_SERVER;
+use magic_wormhole::{AppConfig, transfer, transit, Wormhole};
+use magic_wormhole::transfer::{APPID, TransferError};
+use async_std::{sync::Arc};
+use flutter_rust_bridge::StreamSink;
+use futures::executor::block_on;
+use futures::FutureExt;
 
-// A function definition in Rust. Similar to Dart, the return type must always be named
-// and is never inferred.
-pub fn platform() -> Platform {
-    // This is a macro, a special expression that expands into code. In Rust, all macros
-    // end with an exclamation mark and can be invoked with all kinds of brackets (parentheses,
-    // brackets and curly braces). However, certain conventions exist, for example the
-    // vector macro is almost always invoked as vec![..].
-    //
-    // The cfg!() macro returns a boolean value based on the current compiler configuration.
-    // When attached to expressions (#[cfg(..)] form), they show or hide the expression at compile time.
-    // Here, however, they evaluate to runtime values, which may or may not be optimized out
-    // by the compiler. A variety of configurations are demonstrated here which cover most of
-    // the modern oeprating systems. Try running the Flutter application on different machines
-    // and see if it matches your expected OS.
-    //
-    // Furthermore, in Rust, the last expression in a function is the return value and does
-    // not have the trailing semicolon. This entire if-else chain forms a single expression.
-    if cfg!(windows) {
-        Platform::Windows
-    } else if cfg!(target_os = "android") {
-        Platform::Android
-    } else if cfg!(target_os = "ios") {
-        Platform::Ios
-    } else if cfg!(all(target_os = "macos", target_arch = "aarch64")) {
-        Platform::MacApple
-    } else if cfg!(target_os = "macos") {
-        Platform::MacIntel
-    } else if cfg!(target_family = "wasm") {
-        Platform::Wasm
-    } else if cfg!(unix) {
-        Platform::Unix
-    } else {
-        Platform::Unknown
+pub fn test(sink: StreamSink<String>) -> anyhow::Result<()> {
+    for i in 0..5 {
+        std::thread::sleep(Duration::from_secs(1));
+        sink.add(format!("Hey from rust stream {}", i));
     }
+
+    Ok(())
 }
 
-// The convention for Rust identifiers is the snake_case,
-// and they are automatically converted to camelCase on the Dart side.
-pub fn rust_release_mode() -> bool {
-    cfg!(not(debug_assertions))
+pub enum Events {
+    Code,
+    Total,
+    Sent
+}
+
+pub struct TUpdate {
+    pub event: Events,
+    pub value: String
+}
+
+pub fn send_file(file_name: String, file_path: String, actions: StreamSink<TUpdate>) -> anyhow::Result<()> {
+    block_on(async {
+        match send_filea(file_name, file_path, actions).await {
+            Ok(_) => {}
+            Err(_) => {}
+        }
+    });
+
+    Ok(())
+}
+
+async fn send_filea(file_name: String, file_path: String, actions: StreamSink<TUpdate>) -> Result<(), ()> {
+    let mut relay_hints: Vec<transit::RelayHint> = vec![];
+    if relay_hints.is_empty() {
+        relay_hints.push(transit::RelayHint::from_urls(
+            None,
+            [transit::DEFAULT_RELAY_SERVER
+                .parse()
+                .unwrap()],
+        ).unwrap())
+    }
+
+    // let mut uri_rendezvous =  Some(url::Url::parse(DEFAULT_RENDEZVOUS_SERVER).unwrap());
+    let code_length = 2;
+    let appconfig = AppConfig {
+        id: APPID,
+        rendezvous_url: Cow::from(DEFAULT_RENDEZVOUS_SERVER),
+        app_version: 42,
+    };
+
+    let (server_welcome, connector) =
+        match Wormhole::connect_without_code(appconfig, code_length).await {
+            Ok(v) => v,
+            Err(e) => {
+                println!("{}", e);
+                return Err(())
+            }
+        };
+
+    let code = server_welcome.code;
+
+    actions.add(TUpdate{event: Events::Code, value: code.clone().0});
+    println!("Your code is: {}", code);
+
+    println!("awaiting receiver");
+    let wormhole = match connector.await {
+        Ok(v) => v,
+        Err(e) => {
+            println!("{}", e);
+            return Err(())
+        }
+    };
+
+
+    println!("sending file");
+    match Box::pin(send(
+        wormhole,
+        relay_hints,
+        file_path.as_str(),
+        file_name.as_str(),
+        transit::Abilities::ALL_ABILITIES,
+        actions
+    ))
+        .await {
+        Ok(_) => (),
+        Err(e) => {
+            println!("{}", e);
+            return Err(())
+        }
+    };
+    println!("finished sending");
+    Ok(())
+}
+
+async fn send(
+    wormhole: Wormhole,
+    relay_hints: Vec<transit::RelayHint>,
+    file_path: &str,
+    file_name: &str,
+    transit_abilities: transit::Abilities,
+    actions: StreamSink<TUpdate>
+) -> Result<(), TransferError> {
+    use async_std::sync::{Condvar, Mutex};
+
+    let notifier = Arc::new((Mutex::new(false), Condvar::new()));
+    let test = async move {
+        let (lock, cvar) = &*notifier;
+        let mut started = lock.lock().await;
+        while !*started {
+            started = cvar.wait(started).await;
+        }
+    }
+        .boxed();
+
+    transfer::send_file_or_folder(wormhole, relay_hints, file_path, file_name, transit_abilities, &transit::log_transit_connection, move |sent, total| {
+        if sent == 0 {
+            actions.add(TUpdate{event: Events::Total, value: format!("{}", total)});
+        }
+        // pb.set_position(sent);
+        actions.add(TUpdate{event: Events::Sent, value: format!("{}", sent)});
+    }, test)
+        .await?;
+    // pb2.finish();
+    Ok(())
 }
--- native/src/bridge_generated.web.rs
+++ native/src/bridge_generated.web.rs
@@ -2,13 +2,13 @@ use super::*;
 // Section: wire functions
 
 #[wasm_bindgen]
-pub fn wire_platform(port_: MessagePort) {
-    wire_platform_impl(port_)
+pub fn wire_test(port_: MessagePort) {
+    wire_test_impl(port_)
 }
 
 #[wasm_bindgen]
-pub fn wire_rust_release_mode(port_: MessagePort) {
-    wire_rust_release_mode_impl(port_)
+pub fn wire_send_file(port_: MessagePort, file_name: String, file_path: String) {
+    wire_send_file_impl(port_, file_name, file_path)
 }
 
 // Section: allocate functions
@@ -17,4 +17,31 @@ pub fn wire_rust_release_mode(port_: MessagePort) {
 
 // Section: impl Wire2Api
 
+impl Wire2Api<String> for String {
+    fn wire2api(self) -> String {
+        self
+    }
+}
+
+impl Wire2Api<Vec<u8>> for Box<[u8]> {
+    fn wire2api(self) -> Vec<u8> {
+        self.into_vec()
+    }
+}
 // Section: impl Wire2Api for JsValue
+
+impl Wire2Api<String> for JsValue {
+    fn wire2api(self) -> String {
+        self.as_string().expect("non-UTF-8 string, or not a string")
+    }
+}
+impl Wire2Api<u8> for JsValue {
+    fn wire2api(self) -> u8 {
+        self.unchecked_into_f64() as _
+    }
+}
+impl Wire2Api<Vec<u8>> for JsValue {
+    fn wire2api(self) -> Vec<u8> {
+        self.unchecked_into::<js_sys::Uint8Array>().to_vec().into()
+    }
+}
