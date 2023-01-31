ad7ad8672a3ae197a74f0d3d22ae04c007017e8b
lukas-heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2023-02-01 00:09:44 +0100

    auto close app after share intent if transfer was successful

--- lib/transfer/transfer_receiver.dart
+++ lib/transfer/transfer_receiver.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
@@ -25,7 +26,7 @@ class TransferReceiver extends StatefulWidget {
 class _TransferReceiverState extends State<TransferReceiver> {
   final provider = TransferProvider();
 
-  void _sendFile(String filename, String filepath) async {
+  void _sendFile(String filename, String filepath, bool causedByIntent) async {
     final codeLength = (await Settings.getWordLength()) ?? Defaults.wordlength;
     final stream = api.sendFile(
         fileName: filename, filePath: filepath, codeLength: codeLength);
@@ -34,23 +35,32 @@ class _TransferReceiverState extends State<TransferReceiver> {
     Provider.of<NavigationProvider>(context, listen: false).push(ConnectingPage(
         key: UniqueKey(),
         stream: stream,
-        finish: (file) => Center(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.check_circle_outline,
-                      color: Colors.green,
-                      size: 60,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16),
-                      child: Text(AppLocalizations.of(context)
-                          .transfer_finished_send_label),
-                    ),
-                  ]),
-            )));
+        finish: (file) {
+          // auto close app if share cause was an intent
+          // this happens only if share was successful
+          if (causedByIntent) {
+            Future.delayed(const Duration(seconds: 2))
+                .then((value) => SystemNavigator.pop());
+          }
+
+          return Center(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.check_circle_outline,
+                    color: Colors.green,
+                    size: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text(AppLocalizations.of(context)
+                        .transfer_finished_send_label),
+                  ),
+                ]),
+          );
+        }));
   }
 
   void _receiveFile(String passphrase) async {
@@ -76,7 +86,7 @@ class _TransferReceiverState extends State<TransferReceiver> {
     super.initState();
 
     provider.addOnSendListener((name, path) {
-      _sendFile(name, path);
+      _sendFile(name, path, false);
     });
 
     provider.addOnReceiveListener((passphrase) {
@@ -94,39 +104,39 @@ class _TransferReceiverState extends State<TransferReceiver> {
     );
   }
 
+  /// register handler for intent shares coming from outside of app
   void registerIntentShareHandler() {
-    // For sharing images coming from outside the app
-    // while the app is in the memory
-    ReceiveSharingIntent.getMediaStream().listen(sendIntentFile,
+    ReceiveSharingIntent.getMediaStream().listen(_sendIntentFile,
         onError: (err) {
       debugPrint('$err');
     });
 
     // For sharing images coming from outside the app while the app is closed
-    ReceiveSharingIntent.getInitialMedia().then(sendIntentFile);
+    ReceiveSharingIntent.getInitialMedia().then(_sendIntentFile);
 
     // For sharing or opening urls/text coming from outside the app while the app is in the memory
-    ReceiveSharingIntent.getTextStream().listen(sendIntentText, onError: (err) {
+    ReceiveSharingIntent.getTextStream().listen(_sendIntentText,
+        onError: (err) {
       debugPrint('$err');
     });
 
     // For sharing or opening urls/text coming from outside the app while the app is closed
-    ReceiveSharingIntent.getInitialText().then(sendIntentText);
+    ReceiveSharingIntent.getInitialText().then(_sendIntentText);
   }
 
-  void sendIntentFile(List<SharedMediaFile> value) {
+  void _sendIntentFile(List<SharedMediaFile> value) {
     // todo multiple file not supported for now
     if (value.isEmpty) {
       return;
     }
     final path = value[0].path;
     debugPrint('sending file $path');
-    _sendFile(path.split('/').last, path);
+    _sendFile(path.split('/').last, path, true);
   }
 
-  void sendIntentText(String? value) {
+  void _sendIntentText(String? value) {
     // todo add support sending text
-    if (value != null && value.toString().isNotEmpty) {
+    if (value != null && value.isNotEmpty) {
       debugPrint(value);
     }
   }
