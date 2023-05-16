c3cc8878ff745085c1a1f674537e758b95edff02
lukas-heilgenbrunner <lukas.heiligenbrunner@gmail.com>
2023-05-16 11:17:40 +0200

    rm deprecated size calls

--- lib/utils/device.dart
+++ lib/utils/device.dart
@@ -3,22 +3,13 @@ import 'package:flutter/widgets.dart';
 
 enum DeviceType { phone, tablet }
 
-DeviceType getDeviceType(BuildContext? ctx) {
-  double shortSideLength;
-  // use buildcontext if available
-  if (ctx == null) {
-    final data = MediaQueryData.fromWindow(
-        WidgetsFlutterBinding.ensureInitialized().window);
-    shortSideLength = data.size.shortestSide;
-  } else {
-    shortSideLength = MediaQuery.of(ctx).size.shortestSide;
-  }
-
+DeviceType getDeviceType(BuildContext ctx) {
+  final double shortSideLength = View.of(ctx).physicalSize.shortestSide;
   return shortSideLength < 550 ? DeviceType.phone : DeviceType.tablet;
 }
 
 /// set default device orientation dependent if phone/tablet
-Future<void> setPrefferedAppOrientation({BuildContext? ctx}) async {
+Future<void> setPrefferedAppOrientation({required BuildContext ctx}) async {
   final deviceType = getDeviceType(ctx);
   debugPrint('Device is a: ${deviceType.name}');
 
