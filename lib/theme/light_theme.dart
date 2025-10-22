c0569e310197da9db5f9878acf1f5c97259c3c7a
luksab <emma@sabatschus.de>
2025-10-22 07:54:02 +0200

    Fix light theme as well.

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightTheme = ThemeData(
-    brightness: Brightness.light,
-    colorScheme: ColorScheme.light(
-      surface: Colors.grey[200]!,
-      primary: Colors.grey[400]!,
-      secondary: Colors.grey[300]!,
-      onPrimary: Colors.grey[900]!,
-      onSecondary: Colors.grey[800]!,
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey[200]!,
+    primary: Colors.grey[400]!,
+    secondary: Colors.grey[300]!,
+    onPrimary: Colors.grey[900]!,
+    onSecondary: Colors.grey[800]!,
+  ),
+  appBarTheme: AppBarTheme(backgroundColor: Colors.grey[200]),
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      foregroundColor: Colors.grey[900],
     ),
-    appBarTheme: AppBarTheme(backgroundColor: Colors.grey[200]));
+  ),
+);
