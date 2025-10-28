4677d3ec79990d615ab8a204ddccd7630ab6218e
luksab <emma@sabatschus.de>
2025-10-28 14:12:13 +0100

    Remove import and _buildMobileScannerWidget function from qr_scanner_page in fdroid build

--- scripts/prepare-fdroid-build.sh
+++ scripts/prepare-fdroid-build.sh
@@ -11,11 +11,17 @@ PUBSPEC_FILE="pubspec.yaml"
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' 's/_buildMobileScannerWidget(context)/_buildFlutterZxingWidget(context)/g' "$SCANNER_FILE"
   sed -i '' '/mobile_scanner:/d' "$PUBSPEC_FILE"
+  sed -i '' "/import 'package:mobile_scanner\/mobile_scanner.dart';/d" "$SCANNER_FILE"
+  sed -i '' '/Widget _buildMobileScannerWidget/,/^  }/d' "$SCANNER_FILE"
 else
   sed -i 's/_buildMobileScannerWidget(context)/_buildFlutterZxingWidget(context)/g' "$SCANNER_FILE"
   sed -i '/mobile_scanner:/d' "$PUBSPEC_FILE"
+  sed -i "/import 'package:mobile_scanner\/mobile_scanner.dart';/d" "$SCANNER_FILE"
+  sed -i '/Widget _buildMobileScannerWidget/,/^  }/d' "$SCANNER_FILE"
 fi
 
 echo "F-Droid build preparation complete:"
 echo "  - Replaced _buildMobileScannerWidget with _buildFlutterZxingWidget"
+echo "  - Removed _buildMobileScannerWidget method from qr_scanner_page.dart"
+echo "  - Removed mobile_scanner import from qr_scanner_page.dart"
 echo "  - Removed mobile_scanner dependency from pubspec.yaml"
