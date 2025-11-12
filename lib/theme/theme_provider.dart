5942559581e721eaa28d2303abff30ca283da652
luksab <emma@sabatschus.de>
2025-11-12 13:59:28 +0100

    Implement theme change listener for platform brightness updates

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -7,6 +7,13 @@ enum ThemeType { dark, light, system }
 
 class ThemeProvider with ChangeNotifier {
   ThemeType _theme = ThemeType.dark;
+  late WidgetsBinding _binding;
+
+  ThemeProvider() {
+    _binding = WidgetsBinding.instance;
+    _binding.platformDispatcher.onPlatformBrightnessChanged =
+        _onPlatformBrightnessChanged;
+  }
 
   ThemeType get theme => _theme;
 
@@ -27,5 +34,19 @@ class ThemeProvider with ChangeNotifier {
       case ThemeType.light:
         return false;
     }
+  }
+
+  void _onPlatformBrightnessChanged() {
+    // Only notify listeners if theme mode is set to system
+    // so that system theme changes trigger a rebuild
+    if (_theme == ThemeType.system) {
+      notifyListeners();
+    }
+  }
+
+  @override
+  void dispose() {
+    _binding.platformDispatcher.onPlatformBrightnessChanged = null;
+    super.dispose();
   }
 }
